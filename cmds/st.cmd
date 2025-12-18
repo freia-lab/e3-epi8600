00702959191d7aac70c2aa95dbcbc059b9fb2d36
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2025-12-18 16:58:31 +0100

    Prepared for debugging.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,7 @@
 # This should be a test or example startup script
 
 require epi8600
+
+epicsEnvSet ("IOCNAME", "ioc44-epi8600")
+iocshLoad ("$(epi8600_DIR)epi8600-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.249,TOP=/tmp")
+iocshLoad ("$(epi8600_DIR)epi8600-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=Gasol,ASLO=1,SCALE=1")
