6b6e61c43395a8d8fb13a09dca311ce8e78b5071
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-01-26 17:12:42 +0100

    Version tested at FREIA.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -3,5 +3,5 @@
 require epi8600
 
 epicsEnvSet ("IOCNAME", "ioc44-epi8600")
-iocshLoad ("$(epi8600_DIR)epi8600-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.249,TOP=/tmp")
+iocshLoad ("$(epi8600_DIR)epi8600-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.248,TOP=/tmp")
 iocshLoad ("$(epi8600_DIR)epi8600-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=Gasol,ASLO=1,SCALE=1")
