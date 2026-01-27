3d9bf3f43637eea41065826ee88a18493473ed71
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-01-27 14:31:39 +0100

    Modified startup scripts.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -3,5 +3,15 @@
 require epi8600
 
 epicsEnvSet ("IOCNAME", "ioc44-epi8600")
-iocshLoad ("$(epi8600_DIR)epi8600-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.248,TOP=/tmp")
+epicsEnvSet("IOCDIR", "epi8600")
+
+iocshLoad ("$(epi8600_DIR)epi8600-eth-ctrl.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.248")
 iocshLoad ("$(epi8600_DIR)epi8600-meter.iocsh", "ASYN_PORT_NAME=PORT1,MODBUS_ADR=1,PREFIX=Env-Gas:,DEV_NAME=Gasol,ASLO=1,SCALE=1")
+
+dbLoadRecords("asynRecord.db","P=$(IOCNAME),R=:asynRec,PORT='PORT1',ADDR='0',IMAX='1024',OMAX='256'")
+
+#################################################
+
+iocshLoad("$(autosave_DIR)autosave.iocsh", "AS_TOP=/tmp,IOCNAME=$(IOCNAME)")
+
+#################################################
